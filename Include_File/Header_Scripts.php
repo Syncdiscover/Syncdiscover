98029c774e7ec0974e538e038e92fedbd3823560
Sketcheden <45105913+Sketcheden@users.noreply.github.com>
2021-04-19 16:07:40 +0500

    as

---
+++ Include_File/Header_Scripts.php
@@ -0,0 +1,30 @@
+<?php
+$path  = $_SERVER['DOCUMENT_ROOT']."/";
+$siteurl = "http://syncdiscover.local/";
+?>
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+    <title>Syncdiscover</title>
+
+<meta charset="utf-8">
+<meta name="robots" content="noindex, nofollow">
+<meta name="keywords" content="HTML,CSS,JavaScript">
+<meta name="author" content="">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<link rel="icon" type="image/png" sizes="32x32" href="/assets/sd_32x32.png">
+<link rel="manifest" href="/manifest.json">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+<meta name="theme-color" content="#000">
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" >
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/hover.css/2.3.1/css/hover-min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css">
+<link rel="stylesheet" href="/assets/css/jquery.fancybox.min.css">
+<link rel="stylesheet" href="/assets/css/TimeCircles.css">
+<link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.6.4/css/bootstrap-datepicker.standalone.min.css" rel="stylesheet"/>
+<link type="text/css" rel="stylesheet" href="/dist/style.css">
